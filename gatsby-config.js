9a1ac1e0d4484cdd9c68adc47a5d924f692fb114
Riley <riley@rileybathurst.com>
2022-06-29 16:21:24 -0700

    query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: [
+    'tour'
+  ],
+  singleTypes: [],
+};
+
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://tahoe-city-kayak.netlify.app`
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -15,9 +28,9 @@ module.exports = {
         "path": "./src/images/"
       },
       __key: "images"
-    }]
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },]
 };
-
-
-// localhost strapi API Token
-// 4da35f8b1f502aec723481ad404b801fa780459a2512425d949b2ffe84c88c729861ce238695b52ada85c9f14572a9f784ac1a4b70676be9867116025affffcc23ee6081a96aa74cb2b5cd160181fe2e07419ad39632af3acb1b7c443bdcbd750374b2a65eea84eb7b4fef8d65f2a003979d9f23815f5da8eaccaef4edcca389
--- src/pages/tours-lessons.js
+++ src/pages/tours-lessons.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticQuery, graphql } from 'gatsby';
 
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -13,9 +14,39 @@ const ToursLessonsPage = () => {
         <button>Book Now</button>
         <hr />
       </main>
+
+
+      <StaticQuery
+        query={query}
+        render={data => (
+          <ul className="cards">
+            {
+              data.allStrapiTour.edges.map(tour => (
+                <li key={tour.node.id} className="card">
+                  {tour.node.name}
+                </li>
+              ))
+            }
+          </ul>
+        )}
+      />
+
       <Footer />
     </>
   )
 }
 
 export default ToursLessonsPage
+
+const query = graphql`
+query ToursQuery {
+  allStrapiTour {
+    edges {
+      node {
+        id
+        name
+      }
+    }
+  }
+}
+`
