6f4bf12e829c25227022eb846e51c34deeaa649e
Riley <riley@rileybathurst.com>
2022-07-04 11:21:48 -0700

    sunday build

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,48 @@
-import * as React from "react"
+import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
 
 import Menu from "./menu"
 
+
+function OpenSeason(key, defaultValue) {
+  const [banner, setBanner] = useState('shown');
+
+  useEffect(() => {
+    // setBanner(JSON.parse(window.localStorage.getItem('banner')));
+    setBanner(window.localStorage.getItem('banner'));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('banner', banner);
+  }, [banner]);
+
+  const closeBanner = () => {
+    return setBanner('hidden');
+  }
+
+  const openBanner = () => {
+    return setBanner('shown');
+  }
+
+  return (
+    <>
+      <div className={`top-bar ${banner}`}>
+        <p>We are currently OPEN for 2022’s rental season.</p>
+        <button onClick={closeBanner}>x</button>
+      </div>
+
+      {process.env.NODE_ENV === "development" ? (
+        <button onClick={openBanner}>Put the banner back</button>
+      ) : null}
+
+    </>
+  );
+}
+
 const Header = () => {
   return (
     <header>
-      <div className="top-bar">
-        <p>We are OPEN for 2022&rsquo;s rental season.</p>
-      </div>
+      <OpenSeason />
       <Menu />
       <h1><Link to="/" className="link__subtle">Tahoe City Kayak</Link></h1>
       <h2>North Tahoe&rsquo;s Premier Kayak &amp; SUP Provider of&nbsp;
