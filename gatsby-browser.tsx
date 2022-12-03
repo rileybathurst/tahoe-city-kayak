0612887f490f464877ef85323ee7fca297e5d554
Riley <riley@rileybathurst.com>
2022-12-03 12:59:28 -0800

    comment out

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,5 +3,5 @@ import "./src/styles/styles.scss";
 // import "@fontsource/barlow";  // Defaults to weight 400.
 import "@fontsource/barlow/400.css";
 import "@fontsource/barlow/600.css";
-import "@fontsource/barlow-condensed"; // Defaults to weight 400.
+// import "@fontsource/barlow-condensed"; // Defaults to weight 400.
 import "@fontsource/barlow-condensed/700.css"; // Always used in h tags. which have default bold aka 700 weight.
