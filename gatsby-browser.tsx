771f4d988de824d0350ad250f621630f2c8a5d86
Riley <riley@rileybathurst.com>
2022-12-03 12:59:15 -0800

    font weights

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,7 @@
 import "./src/styles/styles.scss";
 
-import "@fontsource/barlow"; // TODO: only the weights we need
-import "@fontsource/barlow-condensed" // TODO: only the weights we need
+// import "@fontsource/barlow";  // Defaults to weight 400.
+import "@fontsource/barlow/400.css";
+import "@fontsource/barlow/600.css";
+import "@fontsource/barlow-condensed"; // Defaults to weight 400.
+import "@fontsource/barlow-condensed/700.css"; // Always used in h tags. which have default bold aka 700 weight.
