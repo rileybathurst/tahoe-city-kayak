3116ca335be5ca68ec483cb4080ed47a8451ff66
Riley <riley@rileybathurst.com>
2023-02-02 12:55:45 -0800

    correct the names after testing and try remove an unused vaariable

--- src/seo/testimonial.tsx
+++ src/seo/testimonial.tsx
@@ -3,9 +3,12 @@ import { Script } from "gatsby";
 import { useStrapiTestimonial } from "../hooks/use-strapi-testimonial"
 
 export const TestimoialSEO = () => {
-  const {
-    nodes
-  } = useStrapiTestimonial()
+  const { } = useStrapiTestimonial()
+
+  // * this has hard coded data from google rankings dated Feb 2 2023
+  // I dont know if I can get around that as google requires an aggregateRating
+
+  // TODO: This produces double reviews for each, one is correct and one isnt
 
   return (
     <Script type="application/ld+json">
@@ -13,7 +16,7 @@ export const TestimoialSEO = () => {
         {
           "@context": "https://schema.org",
           "@type": "LocalBusiness",
-          "name": "TCK",
+          "name": "Tahoe City Kayak and Padddleboard",
           "aggregateRating": {
             "@type": "AggregateRating",
             "ratingValue": "4.5",
@@ -26,7 +29,7 @@ export const TestimoialSEO = () => {
                 "@type": "Review",
                 "itemReviewed": {
                   "@type": "LocalBusiness",
-                  "name": "K"
+                  "name": "Tahoe City Kayak and Padddleboard"
                 },
                   "author": {
                     "@type": "Person",
