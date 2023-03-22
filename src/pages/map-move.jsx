ecf707bae91dc5868604da2741d825af80825242
Riley <riley@rileybathurst.com>
2023-03-22 16:44:45 -0700

    tone down map-move so its a little smaller

---
+++ src/pages/map-move.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useCallback, useEffect, useMemo } from "react"
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+const center = [51.505, -0.09]
+const zoom = 13
+
+function DisplayPosition({ map }) {
+  const [position, setPosition] = useState(() => map.getCenter())
+
+  const onClick = useCallback(() => {
+    map.setView(center, zoom)
+  }, [map])
+
+  const onMove = useCallback(() => {
+    setPosition(map.getCenter())
+  }, [map])
+
+  useEffect(() => {
+    map.on('move', onMove)
+    return () => {
+      map.off('move', onMove)
+    }
+  }, [map, onMove])
+
+  return (
+    <p>
+      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
+      <button onClick={onClick}>reset</button>
+    </p>
+  )
+}
+
+function ExternalStateExample() {
+  const [map, setMap] = useState(null)
+
+  const displayMap = useMemo(
+    () => (
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        ref={setMap}
+        style={{ height: '400px' }}
+        >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    ),
+    [],
+  )
+
+  return (
+    <>
+      {map ? <DisplayPosition map={map} /> : null}
+      {displayMap}
+    </>
+  )
+}
+
+
+const MovePage = () => {
+  return (
+    <>
+      <Header />
+      <ExternalStateExample />
+      <Footer />
+    </>
+  )
+}
+
+export default MovePage
+
