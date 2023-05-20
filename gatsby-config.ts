6e10208ebb747f3ea776cd8a7bf43f751d3e2b0f
Riley <riley@rileybathurst.com>
2023-05-19 17:46:39 -0700

    ad words

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -143,7 +143,7 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-G787KQM5VC", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "AW-11184788786", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
