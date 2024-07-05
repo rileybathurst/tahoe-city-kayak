926df7e60487cf1df6f2fe25439cf94a26b12a2a
Riley <riley@rileybathurst.com>
2024-07-05 16:59:57 -0700

    paddle ticket test build

--- src/pages/test-tickets.tsx
+++ src/pages/test-tickets.tsx
@@ -3,35 +3,20 @@ import { Link, useStaticQuery, graphql } from 'gatsby';
 
 // Paddle
 import { PaddleLocationDeck } from "@rileybathurst/paddle";
+import { PaddleTicket } from "@rileybathurst/paddle"
+import type { PaddleTicketTypes } from "@rileybathurst/paddle";
 
-import { SEO } from "../components/seo"
-import { useSiteMetadata } from '../hooks/use-site-metadata';
+
 import Header from "../components/header"
 import Footer from "../components/footer"
 import BookNow from "../components/peek/book-now";
 import Experience from "../content/experience";
-import Ticket from "../components/ticket";
-import { CardType } from "../types/card";
 import Sport from "../components/sport";
 
-function Nested(props: { sport: string }) {
-  if (props.sport) {
-    // console.log(props.sport);
-
-    return (
-      <h1 className="capitalize">
-        <Sport sport={props.sport} />
-      </h1>
-    )
-  }
-
-  return null;
-}
-
-const ToursLessonsPage = () => {
+const TestToursLessonsPage = () => {
 
   const query = useStaticQuery(graphql`
-    query ToursQuery {
+    query TestToursQuery {
       kayak: allStrapiTour(
         filter: {
           sport: {eq: "kayak"},
@@ -67,6 +52,10 @@ const ToursLessonsPage = () => {
           nodes {
             ...locationCard
           }
+        }
+
+        strapiLocale(slug: {eq: "tahoe-city"}) {
+          peek_tours
         }
 
       }
@@ -107,9 +96,11 @@ const ToursLessonsPage = () => {
           </hgroup >
 
           <div className="deck">
-            {sport.nodes.map((tour: CardType[]) => (
-              <Ticket
+            {sport.nodes.map((tour: PaddleTicketTypes) => (
+              <PaddleTicket
                 key={tour.id}
+                tour_page='tours-lessons'
+                peek_tours_fall_back={query.strapiLocale.peek_tours}
                 {...tour}
               />
             ))}
@@ -122,13 +113,5 @@ const ToursLessonsPage = () => {
   )
 }
 
-export default ToursLessonsPage
+export default TestToursLessonsPage
 
-export const Head = () => {
-  return (
-    <SEO
-      title={`Tours and Lessons | ${useSiteMetadata().title}`}
-      description="We have many different Kayak Tours to offer, as well as Stand Up Paddleboard Lessons. Our tours leave from multiple locations around the lake."
-    />
-  )
-}
