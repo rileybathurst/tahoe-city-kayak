febbe19ff4a23e333580f8ec1e7eea1229ea0f6c
Riley Bathurst <riley@rileybathurst.com>
2026-05-22 16:05:10 -0700

    updates

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -78,8 +78,41 @@ const AboutPage = () => {
         <hr />
       </main>
 
-      <Footer topHR />
-    </>
+      <section className="pelican">
+        <h3 className="font-serif">
+          <Link to="/about/team">
+            Team
+          </Link>
+        </h3>
+        <p>Meet the team at {data.strapiBranch.name} Kayak & Paddleboard</p>
+        <hr />
+
+        <section className="deck">
+          {data.allStrapiTeam.nodes.map((team: TeamCardTypes) => (
+            <PaddleCard
+              key={team.id}
+              {...team}
+              link={`/about/team/${team.slug}`}
+            />
+          ))}
+        </section>
+
+      </section>
+
+
+      {/* // * specifically using a single here */}
+      <section className="panel denali-padding-block">
+        <h3 className="pelican font-serif">
+          <Link to="/about/testimonials">
+            Testimonials
+          </Link></h3>
+        <ul className='pelican aconcagua-margin-block-end'>
+          <PaddleTestimonial {...data.strapiTestimonial} />
+        </ul>
+      </section>
+
+      <Footer />
+    </React.Fragment>
   )
 }
 
